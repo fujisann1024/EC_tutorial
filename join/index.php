26653c3dd66c58b438233fee66329857ac51fcd1
fujisann1024 <tehepero302@gmail.com>
2020-08-23 10:32:59 +0900

    補足

--- join/index.php
+++ join/index.php
@@ -1,9 +1,22 @@
 <?php
 //error check
-//二回目
+
 if($_POST['name'] === ''){
     $error['name'] = 'nothing';
 }
+if($_POST['address'] === ''){
+    $error['address'] = 'nothing';
+}
+if($_POST['age'] === ''){
+    $error['age'] = 'nothing';
+}
+if($_POST['email'] === ''){
+    $error['email'] = 'nothing';
+}
+if($_POST['password'] === ''){
+    $error['password'] = 'nothing';
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -21,23 +34,41 @@ if($_POST['name'] === ''){
     <div class = "format">
         <div class = "write">次のフォームにご記入ください</div></br>
             <form action="" method = "post">
+            
                 <p>名前<span>※必須</span></p>
                 <input type="text"　name = "name" value="" >
+                <!--エラーメッセージ-->
                 <?php if($error['name'] = 'nothing'):?>
                 <p>名前を入力してください</p>  
                 <?php endif; ?>
                 
                 <p>住所<span>※必須</span></p>
                 <input type="text"　name = "address" value="">
+                    <!--エラーメッセージ-->
+                <?php if($error['name'] = 'nothing'):?>
+                <p>住所を入力してください</p>  
+                <?php endif; ?>
                 
                 <p>年齢<span>※必須</span></p>
                 <input type="text"　name = "age" value="">
+                    <!--エラーメッセージ-->
+                <?php if($error['name'] = 'nothing'):?>
+                <p>年齢を入力してください</p>  
+                <?php endif; ?>
                 
                 <p>メールアドレス <span>※必須</span></p>
                 <input type="text"　name = "email" value="">
+                    <!--エラーメッセージ-->
+                <?php if($error['name'] = 'nothing'):?>
+                <p>メールアドレスを入力してください</p>  
+                <?php endif; ?>
 
                 <p>パスワード<span>※必須</span></p>
                 <input type="text"　name = "password" value="">
+                    <!--エラーメッセージ-->
+                <?php if($error['name'] = 'nothing'):?>
+                <p>パスワードを入力してください</p>  
+                <?php endif; ?>
                 
                 <input type="submit" value="入力を確認する">
                 
