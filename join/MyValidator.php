0c535e8c3c29f94346687bf985dbe60a12f9516a
fujisann1024 <tehepero302@gmail.com>
2020-08-26 22:48:00 +0900

    修正

--- join/MyValidator.php
+++ join/MyValidator.php
@@ -1,28 +1,28 @@
 <?php
 Class MyValidator{
     //エラーメッセージを格納するための変数
-    private $errors;
+    private $_errors;
 
     //コンストラクタ
     public function __construct(string $encoding = 'UTF-8'){
         //プライベート変数$errorsを初期化
-        $errors = [];
+        $_errors = [];
         //内部コードを設定
         mb_internal_encoding($encoding);
         //$_GET,$_POST,$_COOKIEの文字エンコーディングをチェック
-        $this->checkEncoding();
-        $this->checkEncoding();
-        $this->checkEncoding();
+        $this->checkEncoding($_GET);
+        $this->checkEncoding($_POST);
+        $this->checkEncoding($_COOKIE);
         //$_GET,$_POST,$_COOKIEの文字エンコーディングをチェック
-        $this->checkNull();
-        $this->checkNull();
-        $this->checkNull();
+        $this->checkNull($_GET);
+        $this->checkNull($_POST);
+        $this->checkNull($_COOKIE);
     }
     //配列要素に含まれている文字エンコーディングをチェック
     private function checkEncoding(array $data){
         foreach($data as $key => $value){
-            if(!mb_check_encoding($value)){
-                $this->$errors[] = "{$key}は不正な文字コードです";
+            if(!mb_check_encoding($value)){ //与えられた値が内部文字コードと違ったら処理
+                $this->_errors[] = "{$key}は不正な文字コードです";
             }
         }
     }
@@ -31,8 +31,72 @@ Class MyValidator{
     private function checkNull(array $data){
         foreach($data as $key => $value){
             if(preg_match('/\0/',$value)){
-                $this->$errors[] = "{$key}は不正な文字を含んでいます";
+                $this->_errors[] = "{$key}は不正な文字を含んでいます";
             }
         }
     }
+        
+   //必須検証
+    public function requiredCheck(string $value, string $name){
+        if(trim($value) === ''){
+            return '<p style="color:red;">' . $name . "は必須入力です</p>";
+        }
+    }
+
+    //文字列長検証($len文字以内であるか)
+    public function lengthCheck(string $value,string $name,int $len){
+        if(trim($value) !== ''){
+            if(mb_strlen($value) < $len){
+                return '<p style="color:red;">' . $name . "は" . $len . "文字以上で入力してください</p>";
+            }
+        }
+    }
+
+    // //整数型検証
+    // public function intTypeCheck(string $value, string $name){
+    //     if(trim($value) !== ''){
+    //         if(!ctype_digit($value)){
+    //             $this->errors[] = "{$name}は数値で指定してください";
+    //         }
+    //     }
+    // }
+
+    // //数値検証範囲($min~$maxの範囲にあるか)
+    // public function rangeCheck(string $value, string $name, float $max, float $min){
+    //     if(trim($value) !== ''){
+    //         if($value > $max || $value < $min){
+    //             $this->errors[] = "{$name}は{$min}~{$max}で指定してください";
+    //         }
+    //     }
+    // }
+    // //正規表現パターン(パターン$patternに合致するか)
+    // public function regexCheck(string $value, string $name, string $pattern){
+    //     if(trim($value) !== ''){
+    //         if(!preg_match($pattern,$value)){
+    //             $tmp = implode(', ', $opts);
+    //             $this->errors[] = "{$name}は正しい形式で入力してください";
+    //         }
+    //     }
+    // }
+    
+    // //配列要素検証(配列$opsの要素のいずれかであるか)
+    // public function inArrayCheck(string $value, string $name, array $opts){
+    //     if(trim($value) !== ''){
+    //         if(!in_array($value, $opts)){
+    //             $tmp = implode(',',$opts);
+    //             $this->errors[] = "{$name}は{$tmp}の中から選択してください";
+    //         }
+    //     }
+    // }
+
+    //プライベート変数errorsにエラー情報が含まれている場合には表示
+        
 }
+
+$v = new MyValidator();
+$error1_1 = $v->requiredCheck($_POST['name'],"名前");
+$error1_2 = $v->requiredCheck($_POST['address'],"名前");
+$error1_3 = $v->requiredCheck($_POST['age'],"名前");
+$error1_4 = $v->requiredCheck($_POST['email'],"名前");
+$error1_5 = $v->requiredCheck($_POST['password'],"名前");
+$error2 = $v->lengthCheck($_POST['password'],"パスワード",4);
--- join/index.php
+++ join/index.php
@@ -3,27 +3,10 @@
 
 //error check
 require_once("method.php");
-$main = $_POST;
-if(!empty($_POST)){
-    if($_POST['name'] === ''){
-        $error['name'] = 'nothing';
-    }
-    if($_POST['address'] === ''){
-        $error['address'] = 'nothing';
-    }
-    if($_POST['age'] === ''){
-        $error['age'] = 'nothing';
-    }
-    if($_POST['email'] === ''){
-        $error['email'] = 'nothing';
-    }
-    if($_POST['password'] === ''){
-        $error['password'] = 'nothing';
-    }
-    if(strlen($_POST['password']) < 4){
-        $error['password'] = 'nothing';
-    }
-}
+require("MyValidator.php");
+
+
+
 
 
 
@@ -48,36 +31,27 @@ if(!empty($_POST)){
                 <p>名前<span>※必須</span></p>
                 <input type="text" name="name" value="<?php xss($_POST['name']); ?>" >
                 <!--エラーメッセージ-->
-                <!---->
-                <?php emptyError($_POST['name']);?>
+                <?php print $error1;?>
 
                 <p>住所<span>※必須</span></p>
-                <input type="text" name = "address" value="<?php print "Hello"; ?>">
+                <input type="text" name = "address" value="<?php xss($_POST['address']); ?>">
                     <!--エラーメッセージ-->
-                <?php if($error['address'] = 'nothing'):?>
-                <p>住所を入力してください</p>  
-                <?php endif; ?>
+                
                 
                 <p>年齢<span>※必須</span></p>
-                <input type="text" name = "age" value="">
+                <input type="text" name = "age" value="<?php xss($_POST['age']); ?>">
                     <!--エラーメッセージ-->
-                <?php if($error['age'] = 'nothing'):?>
-                <p>年齢を入力してください</p>  
-                <?php endif; ?>
+                
                 
                 <p>メールアドレス <span>※必須</span></p>
-                <input type="text" name = "email" value="">
+                <input type="text" name = "email" value="<?php xss($_POST['email']); ?>">
                     <!--エラーメッセージ-->
-                <?php if($error['email'] = 'nothing'):?>
-                <p>メールアドレスを入力してください</p>  
-                <?php endif; ?>
+                
 
                 <p>パスワード<span>※必須</span></p>
-                <input type="text" name = "password" value="">
+                <input type="text" name = "password" value="<?php xss($_POST['password']); ?>">
                     <!--エラーメッセージ-->
-                <?php if($error['password'] = 'nothing'):?>
-                <p>パスワードを入力してください</p>  
-                <?php endif; ?>
+                    <?php print $error2;?>
                 
                 <input type="submit" value="入力を確認する">
                 
