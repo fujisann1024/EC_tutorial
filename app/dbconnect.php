c67fb8259d2174edbd545352ac723f84b1939fcc
fujisann1024 <tehepero302@gmail.com>
2020-08-30 01:19:34 +0900

    追加

--- app/dbconnect.php
+++ app/dbconnect.php
@@ -1,2 +1,11 @@
 <?php
-$dsn = 'mysql:dbname=EC; host=127.0.0.1; charset=utf8';
+function getDB(){
+    $dsn = 'mysql:dbname=EC; host=127.0.0.1; charset=utf8';
+    $user = 'root';
+    $password = '';
+    //データベースの接続を確立
+    $db = new PDO($dsn.$user,$password,[PDO::ATTR_PERSISTENT => true]);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $db;
+
+}
