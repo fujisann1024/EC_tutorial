b153a6e391a14a57bedd67960f2c01f5b76afaaf
fujisann1024 <tehepero302@gmail.com>
2020-08-30 11:16:04 +0900

    テスト

--- app/dbconnect.php
+++ app/dbconnect.php
@@ -1,11 +1,28 @@
 <?php
-function getDB(){
+
+// $dsn = 'mysql:dbname=EC; host=127.0.0.1; charset=utf8';
+// $user = 'root';
+// $password = '';
+//接続成功確認
+// try{
+//     $db = new PDO($dsn,$user,$password);
+//     print '接続に成功しました';
+// }catch(PDOException $e){
+//     print "接続エラー：{$e->getMessage()}";
+// }finally{
+//     $db = null;
+// }
+function getDb(){
     $dsn = 'mysql:dbname=EC; host=127.0.0.1; charset=utf8';
     $user = 'root';
     $password = '';
-    //データベースの接続を確立
+                                        //持続的接続を有効にするか
     $db = new PDO($dsn.$user,$password,[PDO::ATTR_PERSISTENT => true]);
+    //接続オプション　[エラーの通知方法 => 例外を発生]
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $db;
-
 }
+    
+
+    
+
