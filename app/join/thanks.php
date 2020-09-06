c7b58b48db3b20437fe04bf264b76ce4fbaa5c58
fujisann1024 <tehepero302@gmail.com>
2020-09-06 11:06:56 +0900

    ホーム画面

--- app/join/thanks.php
+++ app/join/thanks.php
@@ -4,8 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+<div class = "format">
     <h1>会員登録完了しました</h1>
+    <a href="../home.html">ホーム画面へ</a>
+</div>
+    
 </body>
 </html>
